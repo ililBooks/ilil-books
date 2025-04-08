93b06a40589b4b11450f962d4c0915a68e27f9b8
queenriwon <queenriwon3@gmail.com>
2025-04-08 21:11:26 +0900

    fix(global): 에러메세지 충돌 수정
    ### 작성한 것
    - 에러메세지 충돌 수정

--- src/main/java/com/example/ililbooks/global/exception/ErrorMessage.java
+++ src/main/java/com/example/ililbooks/global/exception/ErrorMessage.java
@@ -48,9 +48,7 @@ public enum ErrorMessage {
     CART_QUANTITY_INVALID("장바구니 수량은 0 이상이어야 합니다."),
 
     REDIS_PARSING_FAILED("Redis 정보 파싱 실패"),
-    REDIS_SERIALIZE_FAILED("Redis 정보 직렬화 실패"),
-
-    ALREADY_STARTED_EVENT_DELETE_NOT_ALLOWED("이미 시작된 행사는 삭제할 수 없습니다.");
+    REDIS_SERIALIZE_FAILED("Redis 정보 직렬화 실패");
 
     private final String message;
 
