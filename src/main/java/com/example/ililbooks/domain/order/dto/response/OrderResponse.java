774c12961380b1c43f54986d831af9df3949e321
queenriwon <queenriwon3@gmail.com>
2025-04-08 22:42:20 +0900

    feat(order): OrderResponse DTO 작성
    ### 작성한 것
    - OrderResponse DTO 작성

---
+++ src/main/java/com/example/ililbooks/domain/order/dto/response/OrderResponse.java
@@ -0,0 +1,57 @@
+package com.example.ililbooks.domain.order.dto.response;
+
+import com.example.ililbooks.domain.order.entity.Order;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+public class OrderResponse {
+
+    private final Long id;
+
+    private final Long userId;
+
+    private final String number;
+
+    private final BigDecimal totalPrice;
+
+    private final String orderStatus;
+
+    private final String deliveryStatus;
+
+    private final String paymentStatus;
+
+    private final LocalDateTime createdAt;
+
+    private final LocalDateTime modifiedAt;
+
+    @Builder
+    public OrderResponse(Long id, Long userId, String number, BigDecimal totalPrice, String orderStatus, String deliveryStatus, String paymentStatus, LocalDateTime createdAt, LocalDateTime modifiedAt) {
+        this.id = id;
+        this.userId = userId;
+        this.number = number;
+        this.totalPrice = totalPrice;
+        this.orderStatus = orderStatus;
+        this.deliveryStatus = deliveryStatus;
+        this.paymentStatus = paymentStatus;
+        this.createdAt = createdAt;
+        this.modifiedAt = modifiedAt;
+    }
+
+    public static OrderResponse of(Order order) {
+        return OrderResponse.builder()
+                .id(order.getId())
+                .userId(order.getUsers().getId())
+                .number(order.getNumber())
+                .totalPrice(order.getTotalPrice())
+                .orderStatus(order.getOrderStatus().name())
+                .deliveryStatus(order.getDeliveryStatus().name())
+                .paymentStatus(order.getPaymentStatus().name())
+                .createdAt(order.getCreatedAt())
+                .modifiedAt(order.getModifiedAt())
+                .build();
+    }
+}
