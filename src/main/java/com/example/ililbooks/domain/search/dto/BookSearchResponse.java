86e358ddca29a515e876361104b8dadac1752298
Seungmin-J <seungmin103@gmail.com>
2025-04-13 01:33:33 +0900

    feat(search): BookSearchResponse record 변경

--- src/main/java/com/example/ililbooks/domain/search/dto/BookSearchResponse.java
+++ src/main/java/com/example/ililbooks/domain/search/dto/BookSearchResponse.java
@@ -5,26 +5,17 @@ import lombok.Getter;
 
 import java.math.BigDecimal;
 
-@Getter
-public class BookSearchResponse {
-
-    private final String title;
-    private final String author;
-    private final String publisher;
-    private final BigDecimal price;
-    private final String category;
-    private final String saleStatus;
-    private final String limitedType;
-
+public record BookSearchResponse(
+        String title,
+        String author,
+        String publisher,
+        BigDecimal price,
+        String category,
+        String saleStatus,
+        String limitedType
+) {
     @Builder
-    private BookSearchResponse(String title, String author, String publisher, BigDecimal price, String category, String saleStatus, String limitedType) {
-        this.title = title;
-        this.author = author;
-        this.publisher = publisher;
-        this.price = price;
-        this.category = category;
-        this.saleStatus = saleStatus;
-        this.limitedType = limitedType;
+    public BookSearchResponse {
     }
 
 }
